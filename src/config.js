6ab1df4b93d29405dbbaf0132e4f2830e7a6c568
Julien Lengrand-Lambert <julien@lengrand.fr>
2025-02-02 19:46:52 +0100

    Changing default port

--- src/config.js
+++ src/config.js
@@ -70,7 +70,7 @@ function setUpParams() {
     .option('p', {
     alias: 'port',
     description: 'listening port',
-    default: 18000
+    default: 18002
     })
     .option('k', {
     alias: 'key',
